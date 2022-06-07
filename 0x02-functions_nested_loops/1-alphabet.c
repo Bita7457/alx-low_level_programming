8c9c1a073be688eac824313c29fbee27886696cf
Bita <betelhemteshome98@gmail.com>
2022-06-07 06:01:17 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  main - My Code
+ *
+ *  Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
