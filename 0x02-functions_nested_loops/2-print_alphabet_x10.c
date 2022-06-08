7b1689ecf28d5f7b453d135bae6c2a8c1c73c4f5
Bita <betelhemteshome98@gmail.com>
2022-06-08 11:02:53 -0700

    3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,10 @@
-#
-include "main.h"
+#nclude "main.h"include "main.h"
+
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times.
+ * 
+ * Main - Code
  *
  * Return: Always 0 (Success)
- */
-void print_alphabet_x10(vofori){
-	int a, 
+
+{
+	
